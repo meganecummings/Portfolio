3ccbf516e7538292d418122bd9f48ddfdc799e64
meganecummings <megcummings@gmail.com>
2019-09-20 14:40:25 -0700

    Updated GITHUB for Projects

--- src/components/NavBar/NavBar.js
+++ src/components/NavBar/NavBar.js
@@ -6,7 +6,7 @@ import './NavBar.css';
 class NavBar extends Component {
     render() {
         return (
-            <nav className="hamburger fixed-top navbar-expand-lg white">
+            <nav className="hamburger fixed-top navbar-expand-lg white navbar custom-nav sticky">
                 <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarColor01" aria-controls="navbarColor01" aria-expanded="false" aria-label="Toggle navigation">
                     <span className="navbar-toggler-icon"></span>
                 </button>
