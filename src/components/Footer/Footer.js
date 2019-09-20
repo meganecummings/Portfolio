c5a2c89dd2126f7c0d0dc9df5724090e56b71d52
meganecummings <megcummings@gmail.com>
2019-09-19 17:15:58 -0700

    Updated Email Form to Formspree

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -10,8 +10,10 @@ const Footer = () => {
                     <a href=""><i className="fa fa-twitter-square" aria-hidden="true"></i></a>
                     <a href=""><i className="fa fa-instagram" aria-hidden="true"></i></a>
                 </div>
-                <input type="text" placeholder="Email Address" />
-                <button type="submit">Keep in Touch</button>
+                <form className="contact" action="https://formspree.io/megcummings@gmail.com" method="POST"   >
+                    <input type="text" placeholder="Email Address" />
+                    <button type="submit">Keep in Touch</button>
+                </form>
             </div>
             <p className="copyright">&copy;2019 Meg Cummings</p>
         </footer>
