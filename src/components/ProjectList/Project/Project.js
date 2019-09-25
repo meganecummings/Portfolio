a24471dec1105da70121f8de819d372db71cc0f3
meganecummings <megcummings@gmail.com>
2019-09-25 12:16:10 -0700

    Project Card Styling

--- src/components/ProjectList/Project/Project.js
+++ src/components/ProjectList/Project/Project.js
@@ -9,15 +9,9 @@ const Project = () => {
 
         <>
             <div className="project clothingswap">
-                <div className="types">
-                    <div className="react type"><i className="fab fa-react"></i>React</div>
-                    <div className="node type"><i className="fab fa-node"></i> Node.js</div>
-                    <div className="express type"><i className="fas fa-server"></i> Express</div>
-                    <div className="mongoose type"><i className="fas fa-database"></i> Mongoose </div>            
-                </div>
                 <div className="img-container">
-                    <a href="https://meganecummings.github.io/clothingswap-app/" >
-                        <img src={Profile} alt="clothingswap homepage" className="project-img"/>
+                    <a href="https://meganecummings.github.io/clothingswap-app/" target="_blank" rel="noopener noreferrer">
+                        <img src={Profile} alt="clothingswap homepage" className="project-img border"/>
                     </a>
                 </div>
                 <div className="project-body">
@@ -25,31 +19,42 @@ const Project = () => {
                     <div className="project-text">
                         <p>ClothingSwap is MERN stack application for creating and managing your own clothing swap events.</p>
                         <button 
-                            href="https://github.com/meganecummings/clothingswap-app" target="_blank" rel="noopener noreferrer"><i className="fab fa-github-alt fa-i"></i>
+                            href="https://github.com/meganecummings/clothingswap-app" target="_blank" 
+                            rel="noopener noreferrer"><i className="fab fa-github-alt fa-i"></i>
                             Github</button>
+                    </div>
+                    <br/>
+                    <div className="types">
+                        <div className="react type"><i className="fab fa-react"></i>React</div>
+                        <div className="node type"><i className="fab fa-node"></i> Node.js</div>
+                        <div className="express type"><i className="fas fa-server"></i> Express</div>
+                        <div className="mongoose type"><i className="fas fa-database"></i> Mongoose </div>            
                     </div>
                 </div>
             </div>
             <div className="project wayfarer">
-                <div className="types">
-                    <div className="type react"><i className="fab fa-react"></i> React</div>
-                    <div className="type node"><i className="fab fa-node"></i> Node.js</div>
-                    <div className="type express"><i className="fas fa-server"></i> Express</div>
-                    <div className="type mongoose"><i className="fas fa-database"></i> Mongoose </div>
-                </div>
                 <div className="img-container">
                     <a href="https://edfranco.github.io/register">
-                        <img className="project-img" src={Wayfarer} alt="Wayfarer" />
+                        <img className="project-img border" src={Wayfarer} alt="Wayfarer" />
                     </a>
                 </div>
                 <div className="project-body">
                     <h2 className="project-header">Wayfarer</h2>
+                    <div className="project-text">
+                        <p>Wayfarer is a MERN Stack Application that brings weary travelers together to share stories and build connections around their travel discoveries.</p>
+                    </div>
                     <button 
                         href="https://github.com/pouyarezvani/Project-4-Wayfarer" target="_blank"
                         rel="noopener noreferrer"
-                        className="btn btn-custom btn-round"><i className="fab fa-github-alt fa-i"></i>
+                        ><i className="fab fa-github-alt fa-i"></i>
                         Github</button>
-                    <p className="project-text">Wayfarer is a technical concept project based off of rough wireframes from clients. The MERN Stack Application aims to bring weary travelers to share stories and build connections around their discoveries in popular cities.</p>
+                    <br/>
+                    <div className="types">
+                        <div className="type react"><i className="fab fa-react"></i> React</div>
+                        <div className="type node"><i className="fab fa-node"></i> Node.js</div>
+                        <div className="type express"><i className="fas fa-server"></i> Express</div>
+                        <div className="type mongoose"><i className="fas fa-database"></i> Mongoose </div>
+                    </div>
                 </div>
             </div>
         </>
